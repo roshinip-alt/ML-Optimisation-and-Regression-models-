13d9abf5e5869a73cc66b8e09c3bcefbe19d5abd
Roshini <roshinipidikiti@gmail.com>
2026-03-26 11:31:16 +0530

    For future reference

---
+++ Regression_Models/Linear/Linear_regression.py
@@ -0,0 +1,13 @@
+from Minimisation_Methods.Gradient_descent import graddes
+import numpy as np
+
+X=np.array([1,2,3,4])
+Y=np.array([5,6,7,8])
+
+iterations = 1000
+alpha = 0.01
+m,b=graddes(X,Y,iterations,alpha,0,0)
+
+Y_pred=X*m+b
+print(f"Best line of fit: y={m}x+{b}")
+
