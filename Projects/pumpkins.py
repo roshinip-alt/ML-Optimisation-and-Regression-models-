2cc153797f6acc55674f907c857a6ed5b046238d
Roshini <roshinipidikiti@gmail.com>
2026-03-26 21:25:21 +0530

    Update 1

--- Projects/pumpkins.py
+++ Projects/pumpkins.py
@@ -4,8 +4,9 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics import mean_squared_error
 
-pumpkins=pd.read_csv("pumpkins.csv")
+pumpkins = pd.read_csv("../Regression_Models/Dataset/pumpkins.csv")
 price=(pumpkins['Low Price']+pumpkins['High Price'])/2
 month = pd.DatetimeIndex(pumpkins['Date']).month
 # degree 2 testing for fitting
@@ -13,6 +14,10 @@ X= month.values.reshape(-1,1)
 Y= price
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 pipeline=make_pipeline(PolynomialFeatures(2),LinearRegression())
+#linear regression here finds the optimal values ofthe decisionv
 
+pipeline.fit(X_train,Y_train)
 
-
+Y_pred= pipeline.predict(X_test)
+MSE=mean_squared_error(Y_test,Y_pred)
+print(MSE)
